dac5f8b23b3a440445dbf61af0687caf68fd09f8
Faizan Sheikh <faizansheikh223@gmail.com>
2026-07-19 01:10:15 +0530

    s3 backend update

--- backend.tf
+++ backend.tf
@@ -1,17 +1,9 @@
 terraform {
-
   backend "s3" {
-
     bucket         = "terraform-state-faizan-001"
-
     key            = "terraform.tfstate"
-
     region         = "us-east-1"
-
     dynamodb_table = "terraform-locks"
-
     encrypt        = true
-
   }
-
 }
